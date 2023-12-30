3f1440d37ae29673e025ae9cbe7d370224f03061
khalil abbaoui <khalil.abbaoui2002@gmail.com>
2023-12-30 17:01:29 +0100

    corriger la fonction d'addition

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,6 +2,6 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(Calculatrice.add(5, 5));
+		System.out.println("Résultat de l'addition : " + Calculatrice.add(5, 5));
 	}
 }
--- src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -2,6 +2,6 @@ package ma.ehei.calculatrice;
 
 public class Calculatrice {
 	public static int add(int a, int b) {
-		return a-0; // à corriger par a+b
+		return a+b; 
 	}
 }
